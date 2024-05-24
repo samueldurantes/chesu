b0c9d2f4181164378dbfda91d22d227d42ca3b7a
tiagoGott <tiago.panizio@hotmail.com>
2024-05-23 21:21:09 -0300

    set basic chess board
    add chess libs

--- apps/client/app/routes/_index.tsx
+++ apps/client/app/routes/_index.tsx
@@ -1,11 +1,43 @@
+import { useState } from 'react';
 import type { MetaFunction } from '@remix-run/node';
+import { Chessboard } from 'react-chessboard';
+import { Chess, Square } from 'chess.js';
 
 export const meta: MetaFunction = () => {
   return [{ title: 'chesu' }];
 };
 
+interface Move { from: string, to: string, promotion?: string | undefined };
+
 const Index = () => {
-  return <div>chesu</div>;
+  const [game, setGame] = useState(new Chess());
+
+  function playMove(move: string | Move) {
+    const gameCopy = new Chess(game.fen());
+    let result = gameCopy.move(move).san;
+
+    if (result) {
+      setGame(gameCopy);
+      //Send fetch new_move 
+    }
+    return result;
+  }
+
+
+  function onDrop(sourceSquare: Square, targetSquare: Square): boolean {
+    const move = playMove({
+      from: sourceSquare,
+      to: targetSquare,
+    });
+
+    return move != null;
+  }
+
+  return (
+    <div className="container">
+      <Chessboard showPromotionDialog={true} position={game.fen()} boardWidth={600} id=" BasicBoard" onPieceDrop={onDrop} />
+    </div>
+  );
 };
 
 export default Index;
