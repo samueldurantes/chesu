735a8c6e8c7e39272f8dde5f4b60689e4ac3751a
tiagoGott <tiago.panizio@hotmail.com>
2024-05-28 12:55:31 -0300

    add display to search new match

---
+++ apps/client/app/components/radio.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react';
+import { Button } from './ui/button';
+
+type Parameters = {
+  target: string,
+  value: string,
+  fn: Function,
+  children: ReactNode
+}
+
+const Radio = ({ target, value, fn, children }: Parameters) => {
+  return (
+    <Button onClick={() => fn(target)} variant={target != value ? "outline" : undefined}
+      className={`w-40 ${target == value ? "text-white bg-black hover:bg-black" : "hover:bg-black hover:text-white"}`}>
+      {children}
+    </Button >
+  );
+};
+
+export default Radio;
