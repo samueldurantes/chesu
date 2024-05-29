931d9cb46f800a49860e778971803b966ca4d845
Samuel Durante <samueldurante.h@gmail.com>
2024-05-29 02:34:47 +0000

    Deduplicate TailwindCSS config

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -1,48 +1,48 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: ["class"],
+  darkMode: ['class'],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
   ],
-  prefix: "",
+  prefix: '',
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: '2rem',
       screens: {
-        "2xl": "1400px",
+        '2xl': '1400px',
       },
     },
     extend: {
       colors: {
-        'white': '#fff',
-        'black': '#000',
-        'navy': '#003B73',
-        'royal': '#0074B7',
-        'grotto': '#60A3D9',
-        'baby': '#BFD7ED',
+        white: '#fff',
+        black: '#000',
+        navy: '#003B73',
+        royal: '#0074B7',
+        grotto: '#60A3D9',
+        baby: '#BFD7ED',
       },
       keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
         },
       },
       fontFamily: {
-        'kadwa': ['Kadwa', 'serif'],
+        kadwa: ['Kadwa', 'serif'],
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-}
+  plugins: [require('tailwindcss-animate')],
+};
