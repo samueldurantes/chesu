36e59957089591013a1a81cc0d6aded1182f1ba6
Samuel Durante <samueldurante.h@gmail.com>
2024-06-30 17:04:36 +0000

    Add game detail

--- apps/client/src/components/game/Game.tsx
+++ apps/client/src/components/game/Game.tsx
@@ -1,6 +1,176 @@
+import { useEffect, useRef, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import {
+  useSuspenseQueries,
+  useMutation,
+  useQueryClient,
+} from '@tanstack/react-query';
+
+import api from '../../api/api';
+import { Board } from '../ui/Board';
+import { Card } from '../ui/Card';
+import { Button } from '../ui/Button';
+
 const Game = () => {
-	return <div>Game</div>;
+  const connection = useRef<WebSocket | null>(null);
+  const params = useParams();
+
+  const [{ data: queryUser }, { data: queryGame }] = useSuspenseQueries({
+    queries: [
+      {
+        queryKey: ['user/me'],
+        queryFn: () => api.GET('/user/me'),
+      },
+      {
+        queryKey: ['game/detail'],
+        queryFn: async () => {
+          const { data, error } = await api.GET('/game/{id}', {
+            params: {
+              path: {
+                id: params.id as string,
+              },
+            },
+          });
+
+          if (error) {
+            throw new Error(error.message);
+          }
+
+          return data;
+        },
+      },
+    ],
+  });
+
+  const [san, setSan] = useState<string[]>([]);
+
+  useEffect(() => {
+    // TODO: Move this to .env file
+    const socket = new WebSocket('ws://localhost:3000/game/ws');
+
+    socket.addEventListener('open', () => {
+      socket.send(params.id as string);
+    });
+
+    socket.addEventListener('message', (event) => {
+      const move = JSON.parse(event.data);
+
+      console.log({ move });
+
+      setSan((prevSan) => [...prevSan, move]);
+    });
+
+    connection.current = socket;
+
+    return () => {
+      socket.close();
+    };
+  }, [params]);
+
+  const queryClient = useQueryClient();
+
+  const { mutateAsync, isPending } = useMutation({
+    mutationFn: async () => {
+      const { data, error } = await api.POST('/game/{id}', {
+        params: {
+          path: {
+            id: params.id as string,
+          },
+        },
+      });
+
+      if (error) {
+        throw new Error(error.message);
+      }
+
+      console.log({ data });
+
+      return data;
+    },
+    onSuccess: (data) => {
+      queryClient.setQueryData(['game/detail'], data);
+    },
+    onError: (error) => console.log({ error }),
+  });
+
+  const getColorLoggedPlayer = () => {
+    if (queryGame?.game?.white_player?.id === queryUser?.data?.user?.id) {
+      return 'white';
+    }
+
+    return 'black';
+  };
+
+  const colorLoggedPlayer = getColorLoggedPlayer();
+
+  const getOpponent = (): string => {
+    const whitePlayer = queryGame?.game?.white_player;
+    const blackPlayer = queryGame?.game?.black_player;
+
+    if (colorLoggedPlayer === 'white') {
+      if (!blackPlayer) {
+        return 'Waiting for opponent...';
+      }
+
+      return blackPlayer.username;
+    }
+
+    if (!whitePlayer) {
+      return 'Waiting for opponent...';
+    }
+
+    return whitePlayer.username;
+  };
+
+  const getJoinButton = () => {
+    const whitePlayer = queryGame?.game?.white_player;
+    const blackPlayer = queryGame?.game?.black_player;
+
+    if (whitePlayer?.id === queryUser?.data?.user?.id) {
+      return null;
+    }
+
+    if (blackPlayer?.id === queryUser?.data?.user?.id) {
+      return null;
+    }
+
+    return (
+      <Button onClick={() => mutateAsync()} disabled={isPending}>
+        Join
+      </Button>
+    );
+  };
+
+  return (
+    <div className="h-screen flex items-center justify-center gap-2 bg-[#121212]">
+      <div className="flex flex-col gap-4 w-full max-w-[750px] px-6">
+        <Card className="flex items-center justify-between p-4 bg-[#1e1e1e]">
+          <div className="flex gap-2 items-center">
+            <div className="bg-white h-[50px] w-[50px]"></div>
+            <span className="text-white">{getOpponent()}</span>
+          </div>
+        </Card>
+
+        <Board
+          boardOrientation={colorLoggedPlayer}
+          san={san}
+          onMove={(move) => {
+            connection.current?.send(JSON.stringify(move));
+          }}
+        />
+
+        <Card className="flex items-center justify-between p-4 bg-[#1e1e1e]">
+          <div className="flex gap-2 items-center">
+            <div className="bg-white h-[50px] w-[50px]"></div>
+            <span className="text-white">
+              {queryUser?.data?.user?.username}
+            </span>
+          </div>
+          {getJoinButton()}
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default Game;
-
--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+import AuthLayout from './components/auth/AuthLayout.tsx';
 import Home from './components/home/Home.tsx';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
-import AuthLayout from './components/auth/AuthLayout.tsx';
+import Game from './components/game/Game.tsx';
 
 import './index.css';
 
@@ -28,6 +29,10 @@ const router = createBrowserRouter([
         path: '/',
         element: <Home />,
       },
+      {
+        path: '/game/:id',
+        element: <Game />,
+      },
     ],
   },
 ]);
@@ -41,4 +46,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </QueryClientProvider>
   </React.StrictMode>
 );
-
