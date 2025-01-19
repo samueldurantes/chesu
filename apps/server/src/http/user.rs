11d990171ae293417b8faf85c40ed506b8b98878
tiagoGottardo <tiago.panizio@hotmail.com>
2025-01-19 00:29:55 -0300

    refactor me route

--- apps/server/src/http/user.rs
+++ apps/server/src/http/user.rs
@@ -1,7 +1,4 @@
-use crate::{
-    http::{extractor::AuthUser, Result},
-    AppState,
-};
+use crate::http::{extractor::AuthUser, Result};
 use aide::{
     axum::{routing::get_with, ApiRouter},
     transform::TransformOperation,
@@ -9,6 +6,8 @@ use aide::{
 use axum::{extract::State, Json};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use sqlx::Pool;
+use sqlx::Postgres;
 use uuid::Uuid;
 
 use super::error::GenericError;
@@ -23,26 +22,28 @@ struct UserBody<T> {
 }
 
 #[derive(Serialize, Deserialize, JsonSchema)]
-struct User {
-    id: String,
-    username: String,
-    email: String,
+pub struct User {
+    pub id: String,
+    pub username: String,
+    pub email: String,
 }
 
-async fn get_user_by_id(state: &State<AppState>, id: Uuid) -> Result<User, sqlx::Error> {
-    sqlx::query_as!(
-        User,
-        r#" SELECT id, username, email FROM users WHERE id = $1 "#,
-        id,
-    )
-    .fetch_one(&state.db)
-    .await
+impl User {
+    pub async fn from_id(db: &Pool<Postgres>, id: Uuid) -> Result<User, sqlx::Error> {
+        sqlx::query_as!(
+            User,
+            r#" SELECT id, username, email FROM users WHERE id = $1 "#,
+            id,
+        )
+        .fetch_one(db)
+        .await
+    }
 }
 
 async fn me(auth_user: AuthUser, state: State<crate::AppState>) -> Result<Json<UserBody<User>>> {
-    let user = get_user_by_id(&state, auth_user.user_id).await?;
-
-    Ok(Json(UserBody { user }))
+    Ok(Json(UserBody {
+        user: User::from_id(&state.db, auth_user.user_id).await?,
+    }))
 }
 
 fn me_docs(op: TransformOperation) -> TransformOperation {
