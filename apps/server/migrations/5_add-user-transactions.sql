73161238df6743153bdb829182426ff660be9294
tiagoGottardo <tiago.panizio@hotmail.com>
2025-01-21 13:07:12 -0300

    add transactions table

---
+++ apps/server/migrations/5_add-user-transactions.sql
@@ -0,0 +1,8 @@
+CREATE TABLE transactions (
+    id            uuid primary key default uuid_generate_v1mc(),
+    user_id       uuid not null references users(id) on delete cascade,
+    type          text not null check (type in  ('input', 'output')),
+    amount        int not null check (amount > 0),
+    created_at    timestamptz not null default now(),
+    last_balance  int not null check (last_balance >= 0) 
+);
