ee99d6de8434231f17b06665ef6c9b344c26b418
tiagoGottardo <tiago.panizio@hotmail.com>
2025-01-31 19:03:25 -0300

    remove deprecated routes

--- apps/server/src/routes/game/mod.rs
+++ apps/server/src/routes/game/mod.rs
@@ -3,10 +3,8 @@ use aide::axum::{
     ApiRouter,
 };
 
-// mod create_game;
 mod game_handler;
 mod get_game;
-// mod join_game;
 mod quick_pairing_game;
 
 pub fn router() -> ApiRouter {
@@ -15,11 +13,6 @@ pub fn router() -> ApiRouter {
             "/game/pairing",
             post_with(quick_pairing_game::route, quick_pairing_game::docs),
         )
-        // .api_route(
-        //     "/game/create",
-        //     post_with(create_game::route, create_game::docs),
-        // )
-        // .api_route("/game/:id", post_with(join_game::route, join_game::docs))
         .api_route("/game/:id", get_with(get_game::route, get_game::docs))
         .api_route(
             "/game/ws",
