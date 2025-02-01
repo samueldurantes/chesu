40a9a7d1c7baa57b07667d10e0b40912ac698111
tiagoGottardo <tiago.panizio@hotmail.com>
2025-02-01 14:44:57 -0300

    add tests to game model

--- apps/server/src/models/game.rs
+++ apps/server/src/models/game.rs
@@ -105,20 +105,16 @@ impl Game {
         }
 
         let new_game_state = match position.outcome() {
-            Some(Outcome::Decisive { winner }) => match winner {
-                Color::White => Some(GameState::WhiteWin),
-                Color::Black => Some(GameState::BlackWin),
-            },
-            Some(Outcome::Draw) => Some(GameState::Draw),
-            None => None,
-        }
-        .unwrap_or_else(|| {
-            if moves.len() >= 2 {
-                GameState::Running
-            } else {
-                GameState::Waiting
-            }
-        });
+            Some(Outcome::Decisive {
+                winner: Color::White,
+            }) => GameState::WhiteWin,
+            Some(Outcome::Decisive {
+                winner: Color::Black,
+            }) => GameState::BlackWin,
+            Some(Outcome::Draw) => GameState::Draw,
+            _ if moves.len() >= 2 => GameState::Running,
+            _ => GameState::Waiting,
+        };
 
         if self.state == new_game_state {
             return Ok(None);
@@ -128,7 +124,7 @@ impl Game {
     }
 }
 
-#[derive(Default, Serialize, Deserialize, Clone, JsonSchema, PartialEq)]
+#[derive(Default, Serialize, Deserialize, Clone, JsonSchema, PartialEq, Debug)]
 pub enum GameState {
     #[serde(rename = "waiting")]
     #[default]
@@ -141,4 +137,85 @@ pub enum GameState {
     WhiteWin,
     #[serde(rename = "black_win")]
     BlackWin,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::models::game::{Game, GameState};
+    use uuid::Uuid;
+
+    impl Game {
+        fn new() -> Self {
+            Self {
+                id: Uuid::new_v4(),
+                white_player: Uuid::new_v4(),
+                black_player: Uuid::new_v4(),
+                ..Default::default()
+            }
+        }
+
+        fn init_moves(&mut self, moves: Vec<&str>) {
+            self.moves = moves.into_iter().map(|mv| mv.to_string()).collect();
+        }
+    }
+
+    #[test]
+    fn check_move_valid_move() {
+        let mut game = Game::new();
+        game.init_moves(vec!["e4"]);
+        let new_game_state = game.check_move(String::from("c5")).ok();
+
+        assert_eq!(new_game_state, Some(Some(GameState::Running)))
+    }
+
+    #[test]
+    fn check_move_not_new_game_state() {
+        let mut game = Game::new();
+        game.init_moves(vec!["e4", "c5"]);
+        game.state = GameState::Running;
+
+        let new_game_state = game.check_move(String::from("a3")).ok();
+
+        assert_eq!(new_game_state, Some(None))
+    }
+
+    #[test]
+    fn check_move_invalid_move() {
+        let mut game = Game::new();
+        game.init_moves(vec!["e4", "c5", "a3"]);
+        game.state = GameState::Running;
+
+        let new_game_state = game.check_move(String::from("Ra7")).ok();
+
+        assert_eq!(new_game_state, None)
+    }
+
+    #[test]
+    fn check_move_checkmate() {
+        let mut game = Game::new();
+        game.init_moves(vec!["e4", "e5", "Bc4", "a6", "Qf3", "a5"]);
+        game.state = GameState::Running;
+
+        let new_game_state = game.check_move(String::from("Qxf7#")).ok();
+
+        assert_eq!(new_game_state, Some(Some(GameState::WhiteWin)))
+    }
+    #[test]
+
+    fn check_move_draw() {
+        let mut game = Game::new();
+        game.init_moves(vec![
+            "e4", "d5", "exd5", "e6", "dxe6", "fxe6", "Nf3", "g5", "Nxg5", "Qxg5", "Nc3", "e5",
+            "f4", "exf4", "d4", "f3", "gxf3", "Qg6", "Bd3", "Qg7", "f4", "Nc6", "Be3", "Bg4",
+            "Be2", "Bxe2", "Qxe2", "O-O-O", "O-O-O", "Bb4", "Bd2", "Bxc3", "Bxc3", "Nf6", "d5",
+            "Ne7", "Rhe1", "Rhe8", "Qe6+", "Kb8", "d6", "cxd6", "Rxd6", "Rxd6", "Qxd6+", "Ka8",
+            "Bxf6", "Qf7", "Bxe7", "Rxe7", "Rxe7", "Qxe7", "Qxe7", "h5", "h4", "a5", "a4", "b5",
+            "b3", "bxa4", "bxa4", "Kb8", "Qd7", "Ka8",
+        ]);
+        game.state = GameState::Running;
+
+        let new_game_state = game.check_move(String::from("Qc7")).ok();
+
+        assert_eq!(new_game_state, Some(Some(GameState::Draw)))
+    }
 }
