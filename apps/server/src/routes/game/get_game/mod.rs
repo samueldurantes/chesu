c394a48af8a3eb428e77a3ebc9a364c75f94891f
tiagoGottardo <tiago.panizio@hotmail.com>
2025-02-01 09:58:58 -0300

    create get game service

--- apps/server/src/routes/game/get_game/mod.rs
+++ apps/server/src/routes/game/get_game/mod.rs
@@ -1,20 +1,18 @@
 use crate::{
     http::Result,
-    models::{
-        game::Player,
-        rooms_manager::{RoomsManager, RoomsManagerTrait},
-    },
-    repositories::game_repository::{GameRepository, GameRepositoryTrait, GameWithPlayers},
-    Error,
+    models::rooms_manager::RoomsManager,
+    repositories::game_repository::{GameRepository, GameWithPlayers},
 };
 use aide::transform::TransformOperation;
-use anyhow::anyhow;
 use axum::{extract::Path, Json};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use service::GetGameService;
 use uuid::Uuid;
 
 use crate::http::error::GenericError;
+
+mod service;
 
 #[derive(Serialize, Deserialize, JsonSchema)]
 pub struct GameId {
@@ -26,50 +24,15 @@ pub struct GameBody {
     pub game: GameWithPlayers,
 }
 
-fn resource() -> (GameRepository, RoomsManager) {
-    (GameRepository::new(), RoomsManager::new())
-}
-
-fn get_mocked_player() -> Player {
-    Player {
-        id: Uuid::new_v4(),
-        username: String::from("Waiting player..."),
-        email: String::new(),
-    }
+fn resource() -> GetGameService<GameRepository, RoomsManager> {
+    GetGameService::new(GameRepository::new(), RoomsManager::new())
 }
 
 pub async fn route(Path(GameId { id: game_id }): Path<GameId>) -> Result<Json<GameBody>> {
-    let (game_repository, rooms_manager) = resource();
-
-    let room = rooms_manager.get_room(game_id);
-
-    if let Some(room) = room {
-        if !room.is_full() {
-            let (white_player, black_player) = match (room.white_player, room.black_player) {
-                (Some(player_id), None) => Ok((
-                    game_repository.get_player(player_id).await?,
-                    get_mocked_player(),
-                )),
-                (None, Some(player_id)) => Ok((
-                    get_mocked_player(),
-                    game_repository.get_player(player_id).await?,
-                )),
-                _ => Err(Error::Anyhow(anyhow!(""))),
-            }?;
-
-            return Ok(Json(GameBody {
-                game: GameWithPlayers {
-                    id: game_id,
-                    white_player,
-                    black_player,
-                    ..Default::default()
-                },
-            }));
-        }
-    }
+    let get_game_service = resource();
 
     Ok(Json(GameBody {
-        game: game_repository.get_game_with_players(game_id).await?,
+        game: get_game_service.execute(game_id).await?,
     }))
 }
 
---
+++ apps/server/src/routes/game/get_game/service.rs
@@ -0,0 +1,62 @@
+use anyhow::anyhow;
+use uuid::Uuid;
+
+use crate::http::{Error, Result};
+use crate::models::game::Player;
+use crate::models::rooms_manager::RoomsManagerTrait;
+use crate::repositories::game_repository::GameRepositoryTrait;
+use crate::repositories::game_repository::GameWithPlayers;
+
+pub struct GetGameService<R: GameRepositoryTrait, M: RoomsManagerTrait> {
+    game_repository: R,
+    rooms_manager: M,
+}
+
+fn get_mocked_player() -> Player {
+    Player {
+        id: Uuid::new_v4(),
+        username: String::from("Waiting player..."),
+        email: String::new(),
+    }
+}
+
+impl<R: GameRepositoryTrait, M: RoomsManagerTrait> GetGameService<R, M> {
+    pub fn new(game_repository: R, rooms_manager: M) -> Self {
+        Self {
+            game_repository,
+            rooms_manager,
+        }
+    }
+
+    pub async fn execute(&self, room_id: Uuid) -> Result<GameWithPlayers> {
+        let room = self.rooms_manager.get_room(room_id);
+
+        if let Some(room) = room {
+            if !room.is_full() {
+                let (white_player, black_player) = match (room.white_player, room.black_player) {
+                    (Some(player_id), None) => Ok((
+                        self.game_repository.get_player(player_id).await?,
+                        get_mocked_player(),
+                    )),
+                    (None, Some(player_id)) => Ok((
+                        get_mocked_player(),
+                        self.game_repository.get_player(player_id).await?,
+                    )),
+                    _ => Err(Error::Anyhow(anyhow!(
+                        "Something went wrong on room {}",
+                        room_id
+                    ))),
+                }?;
+
+                return Ok(GameWithPlayers {
+                    id: room_id,
+                    white_player,
+                    black_player,
+                    ..Default::default()
+                });
+            }
+        }
+
+        Ok(self.game_repository.get_game_with_players(room_id).await?)
+    }
+}
